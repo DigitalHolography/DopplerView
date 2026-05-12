fix(outlier signal): use boolean indexing instead of trim_zeros, and median_beat instead of average_beat. WIP : freezes when computing temporal cues$

dopplerview/pipeline/steps/pulse_analysis.py
@@ -50,6 +50,7 @@ class PreArteryMaskStep(BaseStep):
         ctx.output_manager.output("pulse_analysis", "labeled_vessels", labeled_vessels, "labeled_mask")
         signals_n = (signals - signals.mean(axis=1, keepdims=True)) / signals.std(axis=1, keepdims=True)
         ctx.set("branch_signals", signals_n)
+        ctx.output_manager.save_h5("branch_signals", ctx)
 
         # --- Step 3: Correct signals by aligning with median heartbeat ---
         beat_period = pulse_analysis.compute_idx0(signals_n, sampling_frequency)
@@ -105,6 +106,15 @@ class ComputeTemporalCuesStep(BaseStep):
         arterial_pulse_interpolated, video_cleaned, beat_signal = pulse_analysis.remove_bad_beats(arterial_pulse_filtered, video, beat_period, threshold=0.8)
         ctx.output_manager.output("pulse_analysis", f"pre_arterial signal corrected", (arterial_pulse_interpolated, beat_signal), "signal", options={"multiple_signals": True, "legend": ["Original Signal", "beat signal"]})
 
+        ctx.set("pre_arterial_pulse", arterial_pulse)
+        ctx.set("pre_venous_pulse", venous_pulse)
+        ctx.set("choroidal_pulse", choroidal_pulse)
+        ctx.set("pre_arterial_pulse_filtered", arterial_pulse_filtered)
+        ctx.set("pre_venous_pulse_filtered", venous_pulse_filtered)
+        ctx.set("pre_arterial_pulse_interpolated", arterial_pulse_interpolated)
+        ctx.set("choroidal_pulse_filtered", choroidal_pulse_filtered)
+
+        ctx.output_manager.save_h5("pre_arterial_pulse_filtered", ctx)
 
         # --- Interpolate outlier frames using the filtered signal ---
 
@@ -122,11 +132,4 @@ class ComputeTemporalCuesStep(BaseStep):
 
         diasys, M0_Systole_img, M0_Diastole_img = pulse_analysis.compute_diasys_image(video_cleaned, arterial_pulse_interpolated, sampling_frequency)
 
-        ctx.set("pre_arterial_pulse", arterial_pulse)
-        ctx.set("pre_venous_pulse", venous_pulse)
-        ctx.set("choroidal_pulse", choroidal_pulse)
-        ctx.set("pre_arterial_pulse_filtered", arterial_pulse_filtered)
-        ctx.set("pre_venous_pulse_filtered", venous_pulse_filtered)
-        ctx.set("pre_arterial_pulse_interpolated", arterial_pulse_interpolated)
-        ctx.set("choroidal_pulse_filtered", choroidal_pulse_filtered)
         ctx.set("diasys_image", diasys)

dopplerview/segmentation/pulse_analysis.py
@@ -172,14 +172,14 @@ def remove_bad_beats(signal, video, beat_period, threshold=0.5):
     """Remove frames on the video corresponding to bad beats from the signal based on correlation with the average beat."""
     peaks = get_peaks(signal, beat_period)
     beats = get_beats(signal, peaks)
-    average_beat = np.nanmean(beats, axis=0)
+    median_beat = np.nanmedian(beats, axis=0)
 
     # Compute correlation of each beat with the average beat
-    correlations = np.array([np.corrcoef(beat, average_beat)[0, 1] for beat in beats])
+    correlations = np.array([np.corrcoef(beat, median_beat)[0, 1] for beat in beats])
     good_beats_mask = correlations > threshold
 
     # Create a cleaned signal by keeping only good beats
-    beat_signal = get_pseudo_signal(average_beat, peaks, len(signal))
+    beat_signal = get_pseudo_signal(median_beat, peaks, len(signal))
     cleaned_signal = np.zeros_like(signal)
     cleaned_video = np.zeros_like(video)
     for i, is_good in enumerate(good_beats_mask):
@@ -189,13 +189,13 @@ def remove_bad_beats(signal, video, beat_period, threshold=0.5):
             cleaned_video[start:end] = video[start:end]
         else:
             start, end = peaks[i], peaks[i + 1]
-            cleaned_signal[start:end] = np.nan
-            beat_signal[start:end] = np.nan
+            cleaned_signal[start:end] = 0
+            beat_signal[start:end] = 0
 
     
-    cleaned_signal = np.trim_zeros(cleaned_signal)
-    cleaned_video = np.trim_zeros(cleaned_video, axis=0)
-    beat_signal = np.trim_zeros(beat_signal)
+    cleaned_signal = cleaned_signal[cleaned_signal != 0]
+    cleaned_video = cleaned_video[np.any(cleaned_video != 0, axis=1)]
+    beat_signal = beat_signal[beat_signal != 0]
 
     return cleaned_signal, cleaned_video, beat_signal
 