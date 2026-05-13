fix(outliers): don't interpolate video. Use cleaned signal for correlation, diasys and M0_ff_image computation. Good performances. Possible tkinter threading problem

dopplerview/pipeline/steps/pulse_analysis.py
@@ -1,7 +1,9 @@
 from dopplerview.pipeline.step import BaseStep, NestedStep
 from dopplerview.segmentation import process_masks, pulse_analysis, signal_processing
 from dopplerview.utils.parallelization_utils import run_in_parallel
+import dopplerview.utils.image_utils as image_utils
 from functools import partial
+
 
 import numpy as np
 
@@ -55,7 +57,7 @@ class PreArteryMaskStep(BaseStep):
         # --- Step 3: Correct signals by aligning with median heartbeat ---
         beat_period = pulse_analysis.compute_idx0(signals_n, sampling_frequency)
         corrected_signals = np.zeros_like(signals_n)
-        func = partial(pulse_analysis.correct_branch_signal_with_heartbeat, beat_period=beat_period, k=8)
+        func = partial(pulse_analysis.correct_branch_signal_with_heartbeat, beat_period=beat_period, k=5)
         corrected_signals = run_in_parallel(func, signals_n, n_jobs=ctx.dopplerview_config["NumberOfWorkers"], chunking=False, task_name="signal correction")
         # for i, signal in enumerate(signals_n):
         #     corrected_signals[i, :] = pulse_analysis.correct_branch_signal_with_heartbeat(signal, beat_period, k=10)
@@ -72,7 +74,7 @@ class PreArteryMaskStep(BaseStep):
 
 class ComputeTemporalCuesStep(BaseStep):
     requires = {"M0_ff_video", "pre_artery_mask", "choroidal_vessel_mask", "beat_period"}
-    produces = {"correlation", "diasys_image", "pre_arterial_pulse", "choroidal_pulse", "pre_arterial_pulse_filtered", "choroidal_pulse_filtered", "pre_arterial_pulse_interpolated", "pre_venous_pulse", "pre_venous_pulse_filtered"}
+    produces = {"correlation", "diasys_image", "pre_arterial_pulse", "choroidal_pulse", "pre_arterial_pulse_filtered", "choroidal_pulse_filtered", "pre_arterial_pulse_interpolated", "pre_venous_pulse", "pre_venous_pulse_filtered", "M0_ff_image_cleaned"}
     name = "temporal_cues"
 
     def _relevant_config(self, ctx):
@@ -104,6 +106,7 @@ class ComputeTemporalCuesStep(BaseStep):
 
         beat_period = ctx.require("beat_period")
         arterial_pulse_interpolated, video_cleaned, beat_signal = pulse_analysis.remove_bad_beats(arterial_pulse_filtered, video, beat_period, threshold=0.8)
+        M0_ff_image_cleaned = image_utils.normalize_to_uint8(np.mean(video_cleaned, axis=0))
         self.logger.info(f"{arterial_pulse_interpolated.shape=}, {beat_signal.shape=}, {video_cleaned.shape=}, {video.shape=}")
         ctx.output_manager.output("pulse_analysis", f"pre_arterial signal corrected", (arterial_pulse_interpolated, beat_signal), "signal", options={"multiple_signals": True, "legend": ["Original Signal", "beat signal"]})
 
@@ -114,12 +117,13 @@ class ComputeTemporalCuesStep(BaseStep):
         ctx.set("pre_venous_pulse_filtered", venous_pulse_filtered)
         ctx.set("pre_arterial_pulse_interpolated", arterial_pulse_interpolated)
         ctx.set("choroidal_pulse_filtered", choroidal_pulse_filtered)
+        ctx.set("M0_ff_image_cleaned", M0_ff_image_cleaned)
 
         ctx.output_manager.save_h5("pre_arterial_pulse_filtered", ctx)
 
         # --- Interpolate outlier frames using the filtered signal ---
 
-        video_cleaned, arterial_pulse_interpolated = signal_processing.interpolate_outliers(video, arterial_pulse, pre_artery_mask, sampling_frequency=sampling_frequency)
+        # video_cleaned, arterial_pulse_interpolated = signal_processing.interpolate_outliers(video, arterial_pulse, pre_artery_mask, sampling_frequency=sampling_frequency)
         # ctx.output_manager.output("pulse_analysis", "video_cleaned", video_cleaned, "video")
 
         # --- Compute correlation map with filtered pulses ---
@@ -131,6 +135,7 @@ class ComputeTemporalCuesStep(BaseStep):
 
         # --- Accumulate frames at the systolic and diastolic peaks of the filtered pulses ---
 
-        diasys, M0_Systole_img, M0_Diastole_img = pulse_analysis.compute_diasys_image(video_cleaned, arterial_pulse_interpolated, sampling_frequency)
+        diasys, sysindexes, diasindexes = pulse_analysis.compute_diasys_image(video_cleaned, arterial_pulse_interpolated, sampling_frequency)
+        ctx.output_manager.output("pulse_analysis", f"diasys plot", (arterial_pulse_interpolated, sysindexes), "signal", options={"scatter": True})
 
         ctx.set("diasys_image", diasys)

dopplerview/segmentation/pulse_analysis.py
@@ -162,7 +162,7 @@ def correct_branch_signal_with_heartbeat(signal, beat_period, k=2):
     signal_length = len(signal)
     peaks = get_peaks(signal, beat_period)
     beats = get_beats(signal, peaks, target_len=signal_length)
-    average_beat = np.nanmean(beats, axis=0)
+    average_beat = np.nanmedian(beats, axis=0)
 
     pseudo_signal = get_pseudo_signal(average_beat, peaks, signal_length)
     corrected_signal = correct_signal(signal, pseudo_signal, k=k)
@@ -642,10 +642,10 @@ def compute_diasys(video, pulse_artery, sampling_frequency, pulse_vein=None):
     return M0_Systole_img, M0_Diastole_img, sysindexes, diasindexes
 
 def compute_diasys_image(video, pulse_artery, sampling_frequency, pulse_vein=None):
-    M0_Systole_img, M0_Diastole_img, _, _, = compute_diasys(video, pulse_artery, sampling_frequency=sampling_frequency, pulse_vein=pulse_vein)
+    M0_Systole_img, M0_Diastole_img, sysindexes, diasindexes = compute_diasys(video, pulse_artery, sampling_frequency=sampling_frequency, pulse_vein=pulse_vein)
 
     sys = image_utils.normalize_image(M0_Systole_img)
     dias = image_utils.normalize_image(M0_Diastole_img)
     diasys_image = image_utils.normalize_image(sys - dias)
-    return diasys_image, M0_Systole_img, M0_Diastole_img
+    return diasys_image, sysindexes, diasindexes
  

dopplerview/utils/parallelization_utils.py
@@ -43,15 +43,19 @@ def run_in_parallel(func, iterable, n_jobs=-1, chunking=True, task_name=None):
         chunks = [[iterable[i] for i in idx] for idx in indices]
 
         # Use threading backend to avoid spawning a new .exe when using multiprocessing in a PyInstaller executable
+        try:
+            results = joblib.Parallel(n_jobs=n_jobs, backend="threading")(
+                joblib.delayed(_process_chunk)(chunk, func)
+                for chunk in chunks
+            )
+            return np.concatenate(results, axis=0)
+        except KeyboardInterrupt:
+            print("Interrupted cleanly")
+
+    try:
         results = joblib.Parallel(n_jobs=n_jobs, backend="threading")(
-            joblib.delayed(_process_chunk)(chunk, func)
-            for chunk in chunks
-        )
-
-        return np.concatenate(results, axis=0)
-
-    results = joblib.Parallel(n_jobs=n_jobs, backend="threading")(
-        joblib.delayed(func)(item) for item in iterable
-    )
-
-    return np.stack(results, axis=0)
+                joblib.delayed(func)(item) for item in iterable
+            )
+        return np.stack(results, axis=0)
+    except KeyboardInterrupt:
+        print("Interrupted cleanly")