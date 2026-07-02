c1316c9e89027f5b4d7d426895ae7c51516e1419
Marius Dubosc <mariusdubosc@gmail.com>
2026-07-02 18:09:15 +0200

    fix(GUI): spawn child processes that handle their pipeline, to handle errors

--- dopplerview/pipeline/steps/read_moments.py
+++ dopplerview/pipeline/steps/read_moments.py
@@ -5,7 +5,7 @@ from dopplerview.pipeline.step import BaseStep
 
 class ReadMomentsStep(BaseStep):
     requires = {"input_file"}
-    produces = {"moment0", "moment1", "moment2", "band_ratio", "LF_M0", "HF_M0"}
+    produces = {"moment0", "moment1", "moment2", "LF_M0", "HF_M0"}
     name = "read_moments"
 
     def _relevant_config(self, ctx):
@@ -16,61 +16,74 @@ class ReadMomentsStep(BaseStep):
 
     def read_hdf5(self, file_path):
         self.logger.info(f"    - Reading the HDF5 file: {file_path}")
-        M0, M1, M2, band_ratio, LF_M0, HF_M0 = None, None, None, None, None, None
+        M0, M1, M2, LF_M0, HF_M0 = None, None, None, None, None
 
         try:
             with h5py.File(file_path, "r") as f:
+                self.logger.info(f"    - Available datasets in the HDF5 file: {list(f.keys())}")
 
-                dataset_names = list(f.keys())
-
-                if "moment0" in dataset_names:
+                try:
                     self.logger.info("    - Reading the M0 data")
-                    M0 = np.transpose(np.squeeze(np.array(f["moment0"][()])), (0, 2, 1))
-                else:
+                    M0 = np.squeeze(np.array(f["moment0"][()]))
+                except:
                     self.logger.info("Warning: moment0 dataset not found")
 
-                if "moment1" in dataset_names:
+                try:
                     self.logger.info("    - Reading the M1 data")
-                    M1 = np.transpose(np.squeeze(np.array(f["moment1"][()])), (0, 2, 1))
-                else:
+                    M1 = np.squeeze(np.array(f["moment1"][()]))
+                except:
                     self.logger.info("Warning: moment1 dataset not found")
 
-                if "moment2" in dataset_names:
+                try :
                     self.logger.info("    - Reading the M2 data")
-                    M2 = np.transpose(np.squeeze(np.array(f["moment2"][()])), (0, 2, 1))
-                else:
+                    M2 = np.squeeze(np.array(f["moment2"][()]))
+                except:
                     self.logger.info("Warning: moment2 dataset not found")
 
-                if "LF_M0" in dataset_names:
-                    self.logger.info("    - Reading the LF_M0 data")
-                    LF_M0 = np.transpose(np.squeeze(np.array(f["LF_M0"][()])), (0, 2, 1))
-                else:
-                    self.logger.info("Warning: LF_M0 dataset not found")
+                self.logger.info("    - Reading the LF_M0 data")
+                try:
+                    lf_m0_data = f["LF_M0"][()]
+                except:
+                    try:
+                        lf_m0_data = f["band_3000_9000"][:]
+                    except:
+                        self.logger.info("Warning: LF_M0 dataset not found")
+                        lf_m0_data = None
+                if lf_m0_data is not None:
+                    LF_M0 = np.squeeze(np.array(lf_m0_data))
                 
-                if "HF_M0" in dataset_names:
-                    self.logger.info("    - Reading the HF_M0 data")
-                    HF_M0 = np.transpose(np.squeeze(np.array(f["HF_M0"][()])), (0, 2, 1))
-                else:
-                    self.logger.info("Warning: HF_M0 dataset not found")
-
-                if "band_ratio" in dataset_names:
-                    self.logger.info("    - Reading the band_ratio data")
-                    band_ratio = np.transpose(np.squeeze(np.array(f["band_ratio"][()])), (0, 2, 1))
-                else:
-                    self.logger.info("Warning: band_ratio dataset not found")
+                self.logger.info("    - Reading the HF_M0 data")
+                try:
+                    hf_m0_data = f["HF_M0"][()]
+                except:
+                    try:
+                        hf_m0_data = f["band_16000_18000"][:]
+                    except:
+                        try:
+                            hf_m0_data = f["band_16000_20000"][:]
+                        except:
+                            self.logger.info("Warning: HF_M0 dataset not found")
+                            hf_m0_data = None
+                if hf_m0_data is not None:
+                    HF_M0 = np.squeeze(np.array(hf_m0_data))
 
         except Exception as e:
             self.logger.info(f"ID: {type(e).__name__}")
             raise
 
-        return M0, M1, M2, band_ratio, LF_M0, HF_M0
+        return M0, M1, M2, LF_M0, HF_M0
 
     def run(self, ctx):
         input_file = ctx.require("input_file")
-        M0, M1, M2, band_ratio, LF_M0, HF_M0 = self.read_hdf5(input_file)
+        M0, M1, M2, LF_M0, HF_M0 = self.read_hdf5(input_file)
         ctx.set("moment0", M0)
         ctx.set("moment1", M1)
         ctx.set("moment2", M2)
-        ctx.set("band_ratio", band_ratio)
-        ctx.set("LF_M0", LF_M0)
-        ctx.set("HF_M0", HF_M0)
+        # ctx.set("LF_M0", LF_M0)
+        # ctx.set("HF_M0", HF_M0)
+        if LF_M0 is not None:
+            ctx.set("LF_M0", LF_M0)
+        if HF_M0 is not None:
+            ctx.set("HF_M0", HF_M0)
+
+        ctx.output_manager.output(self.name, "moment0", M0, "video")
